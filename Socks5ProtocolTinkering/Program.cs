b465a0dbbc5eeb0af02499038fb5ae4e15e6004f
nopara73 <adam.ficsor73@gmail.com>
2017-12-22 14:28:12 -0400

    change to latest minor c# version

--- Socks5ProtocolTinkering/Program.cs
+++ Socks5ProtocolTinkering/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Socks5ProtocolTinkering
 {
     class Program
     {
-        static void Main(string[] args)
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        static async Task Main(string[] args)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             Console.WriteLine("Hello World!");
         }
